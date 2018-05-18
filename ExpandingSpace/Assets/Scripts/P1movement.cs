69585e01df41e524c1276c969dd31bb6f0b142f1
JudaHensen <24311@ma-web.nl>
2018-05-18 10:59:50 +0200

    fixed unity project from 3d to 2d

---
+++ ExpandingSpace/Assets/Scripts/P1movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1movement : MonoBehaviour {
+    public float speed = 3;             //Floating point variable to store the player's movement speed.
+    public float jumpHeight = 4;
+    private bool _allowMovement = true;
+    private bool _isonground = true;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D) && _allowMovement)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.A) && _allowMovement)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
+
+        }
+        if (Input.GetKeyDown(KeyCode.W) && _isonground)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            _isonground = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") { _isonground = true; }
+    }
+}
---
+++ ExpandingSpace/Assets/Scripts/p2movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p2movement : MonoBehaviour {
+
+    public float speed = 7;             //Floating point variable to store the player's movement speed.
+    public float jumpHeight = 50;
+    private bool _allowMovement = true;
+    private bool _isonground = true;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.RightArrow) && _allowMovement)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed, Space.World);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && _allowMovement)
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
+
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _isonground)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            _isonground = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") { _isonground = true; }
+    }
+}
+
