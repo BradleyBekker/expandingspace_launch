fd43d9a267f8cf26a4be53c8591f5c8a5f10bafe
JudaHensen <24311@ma-web.nl>
2018-05-24 11:49:15 +0200

    finally added improved movement script

--- ExpandingSpace/Assets/Scripts/P1movement.cs
+++ ExpandingSpace/Assets/Scripts/P1movement.cs
@@ -2,78 +2,117 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P1movement : MonoBehaviour
-{
+public class P1movement : MonoBehaviour {
 
     public float speed = 3;             //Floating point variable to store the player's movement speed.
-    public float jumpHeight = 4;
-    private bool _allowMovement = true;
-    private bool _isonground = true;
+    public float jumpSpeed = 1;
+    //private bool _allowMovement = true;
 
-    [SerializeField] private GameObject playerrocket;
+    // variables for the jump mechanic
+    public float _gravityFactor = 0.03f;
+    public bool _fallDown = true;
+    public bool isJumping;
+    private bool _constantFall;
+    private float _posJump;
+    private bool _isOnGround = false;
 
-    [SerializeField] private GameObject part1;
-    [SerializeField] private GameObject part2;
-    [SerializeField] private GameObject part3;
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-
-        if (Input.GetKeyDown(KeyCode.B))
+        _posJump = -0.025f;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
         {
-            Death();
+            if (_isOnGround)
+            {
+                GetComponent<Rigidbody2D>().MovePosition( new Vector2( (transform.position.x - speed), transform.position.y ));
+            }
+            else if (!_isOnGround)
+            {
+                GetComponent<Rigidbody2D>().MovePosition( new Vector2( (transform.position.x - speed), CalculateJump() ));
+            }
         }
-        if (Input.GetKey(KeyCode.D) && _allowMovement)
-        {
-            transform.Translate(Vector2.right * Time.deltaTime * speed, Space.World);
 
-        }
-        if (Input.GetKey(KeyCode.A) && _allowMovement)
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.left * Time.deltaTime * speed, Space.World);
+            if (_isOnGround)
+            {
+                GetComponent<Rigidbody2D>().MovePosition( new Vector2( (transform.position.x + speed), transform.position.y ));
+            }
+            else if (!_isOnGround)
+            {
+                GetComponent<Rigidbody2D>().MovePosition( new Vector2( (transform.position.x + speed), CalculateJump() ));
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W) && _isOnGround)
+        {
+            if (_isOnGround)
+            {
+                _posJump = 0.175f;
+                _isOnGround = false;
+                isJumping = true;
+            }
+        }
 
-        }
-        if (Input.GetKeyDown(KeyCode.W) && _isonground)
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && !_isOnGround)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
-            _isonground = false;
+            GetComponent<Rigidbody2D>().MovePosition( new Vector2 (transform.position.x, CalculateJump() ));
         }
     }
+
+    private float CalculateJump()
+    {
+        if (!_fallDown)
+        {
+            if(_posJump * (1 - _gravityFactor * 1.25) > 0.007 )
+            {
+                _posJump = _posJump * (1 - _gravityFactor * 1.25f);
+            }
+            else
+            {
+                _posJump = -0.007f;
+                _fallDown = true;
+            }
+            return transform.position.y + _posJump;
+        }
+        else
+        {
+            
+            if (_posJump * (1 - _gravityFactor) > -.07 && !_constantFall)
+            {
+                _posJump = _posJump * (1 + _gravityFactor);
+            }
+            else
+            {
+                _posJump = -.07f;
+                _constantFall = true;
+            }
+            return transform.position.y + _posJump;
+        }  
+    }
+
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "ground") { _isonground = true; }
+        if (collision.gameObject.tag == "ground")
+        {
+            _isOnGround = true;
+            _fallDown = false;
+            isJumping = false;
+            _constantFall = false;
+        }
     }
 
-
-
-
-    private void Death()
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        Vector2 spawn = new Vector2(1.82f, 0.25f);
-        Vector3 item1spawn = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
-        Vector3 item2spawn = new Vector3(transform.position.x + -5, transform.position.y, transform.position.z);
-        Vector3 item3spawn = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-
-        if (playerrocket.GetComponent<P1rocket>().part1 == true)
+        _isOnGround = false;
+        if (!isJumping)
         {
-            playerrocket.GetComponent<P1rocket>().part1 = false;
-            Instantiate(part1, item1spawn, Quaternion.identity);
-
+            _fallDown = true;
         }
-        if (playerrocket.GetComponent<P1rocket>().part2 == true)
-        {
-            playerrocket.GetComponent<P1rocket>().part2 = false;
-            Instantiate(part2, item2spawn, Quaternion.identity);
-
-        }
-        if (playerrocket.GetComponent<P1rocket>().part3 == true)
-        {
-            playerrocket.GetComponent<P1rocket>().part3 = false;
-            Instantiate(part3, item3spawn, Quaternion.identity);
-
-        }
-        transform.position = spawn;
-
     }
 }
